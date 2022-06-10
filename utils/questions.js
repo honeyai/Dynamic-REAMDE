641d1e2bee822c2c4f05070a0651a6cc6ccc6415
honeyai <contact.jessicasanchez@gmail.com>
2022-06-09 23:14:57 -0400

    added more questions and providing validation for questions

--- utils/questions.js
+++ utils/questions.js
@@ -3,31 +3,75 @@ const questions = [
     type: 'input',
     name: 'title',
     message: 'What is the name of your project?',
+    validate: (title) => {
+      if(title) true
+      return 'Please provide a title for your README.'
+    }
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'Enter your full name.',
+    validate: (author) => {
+      if(author) true
+      return 'Please provide your name.'
+    }
   },
   {
     type: 'editor',
     name: 'description',
     message: 'Explain the what, why, and how of your project.',
+    validate: (description) => {
+      if(description) true
+      return 'Please write up the what, why, and how for your project.'
+    }
   },
   {
     type: 'editor',
     name: 'installation',
     message: 'Provide the instructions to install your app.',
+    validate: (installation) => {
+      if(installation) return
+      return 'Please provide instructions for installation.'
+    }
   },
   {
-    type: 'input',
+    type: 'editor',
     name: 'usage',
     message: 'Give examples and instructions on how to use your app.',
+    validate: (usage) => {
+      if(usage) return
+      return 'Please give examples and instructions.'
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'hasCollab',
+    message: 'Do you have collaborators to credit?',
+    default: false,
   },
   {
     type: 'input',
     name: 'collab',
     message: 'List your collaborators if any, with their Github profiles. List like so: name link, name link, etc',
+    when: (answers) => {
+      return answers.hasCollab !== false;
+    },
+    validate: (collab) => {
+      if (collab) {
+        if(collab.length % 2 === 0) return
+        return "You're missing either a name or link to that individual"
+      }
+      return 'List '
+    }
   },
   {
     type: 'input',
     name: 'third party',
     message: 'List any third party assets and their links. List any tutorials too. List like so: Asset link, Asset link, etc',
+    validate: () => {
+
+    }
   },
   {
     type: 'list',
@@ -40,5 +84,8 @@ const questions = [
     type: 'input',
     name: 'username',
     message: 'Provide your github username',
+    validate: () => {
+
+    }
   }
 ];
