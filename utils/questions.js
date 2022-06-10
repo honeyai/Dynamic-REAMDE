7e1a2aec58751d6965a509a3aeb38920c01db160
honeyai <contact.jessicasanchez@gmail.com>
2022-06-09 23:25:57 -0400

    refining questions

--- utils/questions.js
+++ utils/questions.js
@@ -4,7 +4,7 @@ const questions = [
     name: 'title',
     message: 'What is the name of your project?',
     validate: (title) => {
-      if(title) true
+      if (title) true
       return 'Please provide a title for your README.'
     }
   },
@@ -13,7 +13,7 @@ const questions = [
     name: 'author',
     message: 'Enter your full name.',
     validate: (author) => {
-      if(author) true
+      if (author) true
       return 'Please provide your name.'
     }
   },
@@ -22,7 +22,7 @@ const questions = [
     name: 'description',
     message: 'Explain the what, why, and how of your project.',
     validate: (description) => {
-      if(description) true
+      if (description) true
       return 'Please write up the what, why, and how for your project.'
     }
   },
@@ -31,7 +31,7 @@ const questions = [
     name: 'installation',
     message: 'Provide the instructions to install your app.',
     validate: (installation) => {
-      if(installation) return
+      if (installation) return
       return 'Please provide instructions for installation.'
     }
   },
@@ -40,7 +40,7 @@ const questions = [
     name: 'usage',
     message: 'Give examples and instructions on how to use your app.',
     validate: (usage) => {
-      if(usage) return
+      if (usage) return
       return 'Please give examples and instructions.'
     }
   },
@@ -53,24 +53,37 @@ const questions = [
   {
     type: 'input',
     name: 'collab',
-    message: 'List your collaborators if any, with their Github profiles. List like so: name link, name link, etc',
+    message: 'List your collaborators if any, with their Github profiles. List like so: name, link, name, link, etc',
     when: (answers) => {
       return answers.hasCollab !== false;
     },
     validate: (collab) => {
       if (collab) {
-        if(collab.length % 2 === 0) return
-        return "You're missing either a name or link to that individual"
+        if (collab.length % 2 === 0) return
+        return "You're missing either a name or link to that individual."
       }
-      return 'List '
+      return 'List your collaborators separated by a comma.'
     }
   },
   {
+    type: 'confirm',
+    name: 'hasThirdParty',
+    message: 'Did you use third party resources?',
+    default: false,
+  },
+  {
     type: 'input',
-    name: 'third party',
-    message: 'List any third party assets and their links. List any tutorials too. List like so: Asset link, Asset link, etc',
-    validate: () => {
-
+    name: 'thirdParty',
+    message: 'List any third party assets and their links. List any tutorials too. List like so: Asset, link, Asset. link, etc',
+    when: (answers) => {
+      return answers.hasCollab !== false
+    },
+    validate: (thirdParty) => {
+      if (thirdParty) {
+        if (thirdParty % 2 === 0) return
+        return "You're missing either a name or a link to that asset."
+      }
+      return 'List the third party assets you use and the link to that asset.'
     }
   },
   {
